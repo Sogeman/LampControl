b50e8f08e782fc4ab47543c15190c27cad52871f
Rene Mandler <32226747+Sogeman@users.noreply.github.com>
2019-01-23 20:37:43 +0100

    console logs removed

--- LampControlFrontend/src/app/light/light.component.ts
+++ LampControlFrontend/src/app/light/light.component.ts
@@ -21,7 +21,6 @@ export class LightComponent implements OnInit {
   ngOnInit() {
     if (localStorage.getItem('bridgeIp')) {
       this.refreshAllLightsOnly();
-      console.log('all lights retrieved');
     }
   }
 
@@ -29,63 +28,53 @@ export class LightComponent implements OnInit {
     this.hueService.retrieveSingleLight(id)
       .then(light => this.selectedLight = light)
       .then(() => this.id = id);
-    console.log('light id ' + id + ' retrieved');
   }
 
   refreshAllLightsOnly() {
     this.hueService.retrieveAllLights()
       .then(lights => this.lightList = lights);
-    console.log('all lights retrieved');
   }
 
   refreshAll() {
     this.hueService.retrieveAllLights()
       .then(lights => this.lightList = lights);
     this.lightsRefreshed.emit();
-    console.log('all lights retrieved');
   }
 
   saveLightName(value: string, id: number, type: string) {
     this.hueService.changeName(value, id, type)
       .then(() => this.refreshSingleLight(id));
-    console.log('lightname changed');
   }
 
   toggleLight(lightState: boolean, id: number) {
     this.hueService.toggleLight(lightState, id)
       .then(() => this.refreshAll());
-    console.log('light ' + id + ' toggled');
   }
 
   toggleLightDetail(lightState: boolean, id: number) {
     this.hueService.toggleLight(lightState, id)
       .then(() => this.refreshSingleLight(id));
-      console.log('light ' + id + ' toggled');
   }
 
   clearSelectedLight() {
     this.selectedLight = null;
     this.refreshAll();
-    console.log('return to homepage');
   }
 
   changeState(state: string, id: number) {
     const changeState = this.manipulationService.calculateChangeLightState(state);
     this.hueService.updateState('lights', changeState.xy, changeState.bri, id)
       .then(() => this.refreshSingleLight(id));
-    console.log('light ' + id + ' state changed to ' + state);
   }
 
   clearLightSearch() {
     this.lightSearch = false;
     this.refreshAll();
-    console.log('return to homepage');
   }
 
   deleteSelectedLight(id: number) {
     this.hueService.deleteEntity(id, 'lights')
       .then(() => this.clearSelectedLight());
-    console.log('light ' + id + ' deleted and return to homepage');
   }
 
 }
