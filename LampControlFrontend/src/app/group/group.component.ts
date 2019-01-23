a498e08842c7c62fa55e0abeedfa87eb25c537ab
Rene Mandler <32226747+Sogeman@users.noreply.github.com>
2019-01-23 20:35:45 +0100

    console logs removed

--- LampControlFrontend/src/app/group/group.component.ts
+++ LampControlFrontend/src/app/group/group.component.ts
@@ -26,7 +26,6 @@ export class GroupComponent implements OnInit {
   ngOnInit() {
     if (localStorage.getItem('bridgeIp')) {
       this.refreshAllGroupsOnly();
-      console.log('all groups retrieved');
     }
   }
 
@@ -49,32 +48,27 @@ export class GroupComponent implements OnInit {
     this.hueService.retrieveSingleGroup(id)
       .then(group => this.selectedGroup = group)
       .then(() => this.groupId = id);
-    console.log('group ' + id + ' retrieved');
   }
 
   refreshAllGroupsOnly() {
     this.hueService.retrieveAllGroups()
       .then(groups => this.groupList = this.filterGroups(groups));
-      console.log('all groups retrieved');
   }
 
   refreshAll() {
     this.hueService.retrieveAllGroups()
       .then(groups => this.groupList = this.filterGroups(groups));
     this.groupsRefreshed.emit();
-    console.log('all groups retrieved');
   }
 
   toggleGroup(groupState: boolean, id: number) {
     this.hueService.toggleGroup(groupState, id)
       .then(() => this.refreshAll());
-    console.log('group ' + id + ' toggled');
   }
 
   toggleGroupDetail(lightState: boolean, id: number) {
     this.hueService.toggleGroup(lightState, id)
       .then(() => this.refreshSingle(id));
-      console.log('group ' + id + ' toggled');
   }
 
   viewSelectedGroup(id: number) {
@@ -83,10 +77,8 @@ export class GroupComponent implements OnInit {
         if (group.lights.length < 1) {
           this.isChangingLights = true;
           this.groupId = id;
-          console.log('change to light add view');
         } else {
           this.selectedGroup = group;
-          console.log('group ' + id + ' retrieved');
         }
         })
       .then(() => this.groupId = id);
@@ -95,7 +87,6 @@ export class GroupComponent implements OnInit {
   clearSelectedGroup() {
     this.selectedGroup = null;
     this.refreshAll();
-    console.log('return to homepage');
   }
 
 
@@ -103,27 +94,23 @@ export class GroupComponent implements OnInit {
     this.isChangingLights = false;
     this.isGroupCreation = false;
     this.refreshAllGroupsOnly();
-    console.log('return to homepage');
   }
 
   saveGroupName(value: string, id: number, type: string) {
     this.hueService.changeName(value, id, type)
       .then(() => this.refreshSingle(id));
-    console.log('groupname changed');
   }
 
   changeState(state: string, id: number) {
     const changeState = this.manipulationService.calculateChangeLightState(state);
     this.hueService.updateState('groups', changeState.xy, changeState.bri, id)
       .then(() => this.refreshSingle(id));
-    console.log('groupstate ' + id + ' changed');
   }
 
   deleteSelectedGroup(id: number) {
     this.hueService.deleteEntity(id, 'groups')
       .then(() => this.clearSelectedGroup())
       .then(() => this.isChangingLights = false);
-    console.log('group ' + id + ' deleted and return to homepage');
   }
 
   createGroup(body: any) {
