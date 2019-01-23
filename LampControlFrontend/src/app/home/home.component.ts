b81443401c0e8bfbe37ffbd335c9a334665f1029
Rene Mandler <32226747+Sogeman@users.noreply.github.com>
2019-01-23 20:36:23 +0100

    console logs removed

--- LampControlFrontend/src/app/home/home.component.ts
+++ LampControlFrontend/src/app/home/home.component.ts
@@ -19,23 +19,18 @@ export class HomeComponent implements OnInit {
       this.user.username = localStorage.getItem('username');
       this.user.nickname = localStorage.getItem('nickname');
       this.user.bridgeIp = localStorage.getItem('bridgeIp');
-      console.log('Fetched userdata from storage.');
     } else {
       this.hueService.fetchBridgeUrl()
         .then(bridgeIp => {
           localStorage.setItem('bridgeIp', bridgeIp);
           this.user.bridgeIp = bridgeIp;
-          console.log('Saved Bridge IP');
       });
     }
   }
 
   setUser(user: User) {
     this.user = user;
-    console.log('user created');
-    console.log(this.user);
     this.setLocalStorage();
-    console.log('Saved userdata to storage');
   }
 
   setLocalStorage() {
